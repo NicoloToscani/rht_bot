e64862242f1548fe9070e02389958e5268f0d31d
Nicolò Toscani <toscani.nicolo90@gmail.com>
2022-03-27 11:20:48 +0200

    Add files via upload

---
+++ pygame_controller.py
@@ -0,0 +1,173 @@
+import pygame
+ 
+# Define some colors
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+ 
+ 
+class TextPrint(object):
+    """
+    This is a simple class that will help us print to the screen
+    It has nothing to do with the joysticks, just outputting the
+    information.
+    """
+    def __init__(self):
+        """ Constructor """
+        self.reset()
+        self.x_pos = 10
+        self.y_pos = 10
+        self.font = pygame.font.Font(None, 20)
+ 
+    def print(self, my_screen, text_string):
+        """ Draw text onto the screen. """
+        text_bitmap = self.font.render(text_string, True, BLACK)
+        my_screen.blit(text_bitmap, [self.x_pos, self.y_pos])
+        self.y_pos += self.line_height
+ 
+    def reset(self):
+        """ Reset text to the top of the screen. """
+        self.x_pos = 10
+        self.y_pos = 10
+        self.line_height = 15
+ 
+    def indent(self):
+        """ Indent the next line of text """
+        self.x_pos += 10
+ 
+    def unindent(self):
+        """ Unindent the next line of text """
+        self.x_pos -= 10
+ 
+ 
+pygame.init()
+ 
+# Set the width and height of the screen [width,height]
+size = [500, 700]
+screen = pygame.display.set_mode(size)
+ 
+pygame.display.set_caption("My Game")
+ 
+# Loop until the user clicks the close button.
+done = False
+ 
+# Used to manage how fast the screen updates
+clock = pygame.time.Clock()
+ 
+# Initialize the joysticks
+pygame.joystick.init()
+ 
+# Get ready to print
+textPrint = TextPrint()
+
+# -------- Main Program Loop -----------
+while not done:
+    # EVENT PROCESSING STEP
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+ 
+        # Possible joystick actions: JOYAXISMOTION JOYBALLMOTION JOYBUTTONDOWN
+        # JOYBUTTONUP JOYHATMOTION
+        
+        #if event.type == pygame.JOYBUTTONDOWN:
+         #   print("Ricky Bolzoni")
+        #if event.type == pygame.JOYBUTTONUP:
+         #   print("GIovanni Zametta")
+ 
+    # DRAWING STEP
+    # First, clear the screen to white. Don't put other drawing commands
+    # above this, or they will be erased with this command.
+    screen.fill(WHITE)
+    textPrint.reset()
+ 
+    # Get count of joysticks
+    joystick_count = pygame.joystick.get_count()
+ 
+    textPrint.print(screen, "Number of joysticks: {}".format(joystick_count))
+    textPrint.indent()
+ 
+    # For each joystick:
+    for i in range(joystick_count):
+        joystick = pygame.joystick.Joystick(i)
+        joystick.init()
+ 
+        textPrint.print(screen, "Joystick {}".format(i))
+        textPrint.indent()
+ 
+        # Get the name from the OS for the controller/joystick
+        name = joystick.get_name()
+        textPrint.print(screen, "Joystick name: {}".format(name))
+ 
+        # Usually axis run in pairs, up/down for one, and left/right for
+        # the other.
+        axes = joystick.get_numaxes()
+        textPrint.print(screen, "Number of axes: {}".format(axes))
+        textPrint.indent()
+ 
+        for i in range(axes):
+            axis = joystick.get_axis(i)
+            textPrint.print(screen, "Axis {} value: {:>6.3f}".format(i, axis))
+            if(i == 1 and axis == -1):
+                print("freccia alta")
+            elif(i == 1 and axis > 0):
+                print("freccia bassa")
+            elif(i == 0 and axis == -1):
+                print("freccia sinistra")
+            elif(i == 0 and axis > 0):
+                print("freccia destra")
+        textPrint.unindent()
+ 
+        buttons = joystick.get_numbuttons()
+        textPrint.print(screen, "Number of buttons: {}".format(buttons))
+        textPrint.indent()
+ 
+        for i in range(buttons):
+            button = joystick.get_button(i)
+            textPrint.print(screen, "Button {:>2} value: {}".format(i, button))
+            if(i == 0 and button == 1):
+                print("triangolo")
+            elif(i == 1 and button == 1):
+                print('cerchio')
+            elif(i == 2 and button == 1):
+                print("X")
+            elif(i == 3 and button == 1):
+                print("quandrato")
+            elif(i == 4 and button == 1):
+                print("L2")
+            elif(i == 5 and button == 1):
+                print("R2")
+            elif(i == 6 and button == 1):
+                print("L1")
+            elif(i == 7 and button == 1):
+                print("R1")
+            elif(i == 8 and button == 1):
+                print("select")
+            elif(i == 9 and button == 1):
+                print("start")
+        textPrint.unindent()
+ 
+        # Hat switch. All or nothing for direction, not like joysticks.
+        # Value comes back in an array.
+        hats = joystick.get_numhats()
+        textPrint.print(screen, "Number of hats: {}".format(hats))
+        textPrint.indent()
+ 
+        for i in range(hats):
+            hat = joystick.get_hat(i)
+            textPrint.print(screen, "Hat {} value: {}".format(i, str(hat)))
+        textPrint.unindent()
+ 
+        textPrint.unindent()
+ 
+    # ALL CODE TO DRAW SHOULD GO ABOVE THIS COMMENT
+ 
+    # Go ahead and update the screen with what we've drawn.
+    pygame.display.flip()
+ 
+    # Limit to 60 frames per second
+    clock.tick(60)
+ 
+# Close the window and quit.
+# If you forget this line, the program will 'hang'
+# on exit if running from IDLE.
+pygame.quit()
